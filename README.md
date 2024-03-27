acfefe0d8a562840bca360669fbb0d1f71558b95
Gubaz311 <bagusste25@gmail.com>
2024-03-27 15:56:35 +0700

    restart

--- README.md
+++
@@ -1,2 +0,0 @@
-# myOwnUnila_server
-making server from myOwnUnila using go and postgres
---
+++ api/connectDatabase.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func connect() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+}
